fe1f5ba64662eb1b1d47ba0c1cf4a42e7bc629f9
chenjunsen <chenjunsen@csii.com.cn>
2020-08-17 14:28:00 +0800

    增加Activity作为参数

--- app/src/main/java/com/cjs/cjsbridge/web/CJSWebClient.java
+++ app/src/main/java/com/cjs/cjsbridge/web/CJSWebClient.java
@@ -1,7 +1,10 @@
 package com.cjs.cjsbridge.web;
 
+import android.app.Activity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.cjs.cjsbridge.tools.L;
 
 /**
  * 自定义WebViewClient
@@ -13,18 +16,21 @@ import android.webkit.WebViewClient;
  *     <li>onReceiveError()</li>
  *     <li>onReceivedHttpAuthRequest()</li>
  * </ul>
+ *
  * @author JasonChen
  * @email chenjunsen@outlook.com
  * @createTime 2020/8/12 0012 18:03
  */
 public class CJSWebClient extends WebViewClient {
+    Activity activity;
 
-    public CJSWebClient() {
+    public CJSWebClient(Activity activity) {
+        this.activity=activity;
     }
-
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        L.d("拦截到请求地址1:" + url);
         //重写该方法，只需返回false（官方用法），就可以阻止webView在加载url时调用系统浏览器的问题
         return false;
     }
