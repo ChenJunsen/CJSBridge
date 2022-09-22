e994f2ae1a24924adfefb2978bf56668b2524b4a
陈俊森 <022927@sichuanair.com>
2022-09-22 15:27:34 +0800

    修正注入js的时机，解决在雷电模拟器上面无效的bug

--- cjsbridge-advance/src/main/java/com/cjs/cjsbridge_advance/web/CJSWebViewClient.java
+++ cjsbridge-advance/src/main/java/com/cjs/cjsbridge_advance/web/CJSWebViewClient.java
@@ -10,12 +10,14 @@ import com.cjs.cjsbridge_advance.core.exception.CJSBException;
 import com.cjs.cjsbridge_advance.dispatch.CJSActionDispatcher;
 import com.cjs.cjsbridge_common.scheme.CJScheme;
 import com.cjs.cjsbridge_common.scheme.CJSchemeParser;
+import com.cjs.cjsbridge_common.tools.L;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * 进阶版WebClient
+ *
  * @author JasonChen
  * @email chenjunsen@outlook.com
  * @createTime 2020/9/16 0016 17:54
@@ -48,8 +50,19 @@ public class CJSWebViewClient extends WebViewClient {
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        if (cjsActionDispatcher != null) {
+            cjsActionDispatcher.onPageStarted(view, url);
+        }
+    }
+
+    @Override
+    public void onPageFinished(final WebView view, final String url) {
+        super.onPageFinished(view, url);
         //从asset文件夹中读出对应的JS桥文件，然后注入webView当中
         //这样做，可以使得H5端在创建页面的时候不用手动引入相关的js文件，实现对H5端的透明化
+        // 修改于2022-09-22 之前注入js的操作是放在onPageStared里面的。但是在测试的时候，发现雷电模拟器无法执行加载的js
+        // 将注入js的流程切换到onPageFinished就可以了
         try {
             InputStream is = view.getContext().getAssets()
                     .open("advance/CJSBridge2.js");
@@ -58,28 +71,23 @@ public class CJSWebViewClient extends WebViewClient {
             is.read(buffer);
             is.close();
             String js = new String(buffer);
-            executeJavascript(view, js);
+            executeJavascript(view, js, new CJSBridge2.CallH5CallBack() {
+                @Override
+                public void onExecutedJavascript(String value) {
+                    if (cjsActionDispatcher != null) {
+                        cjsActionDispatcher.onPageFinished(view, url);
+                    }
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         } catch (CJSBException e) {
             e.printStackTrace();
         }
-        super.onPageStarted(view, url, favicon);
-        if(cjsActionDispatcher!=null){
-            cjsActionDispatcher.onPageStarted(view,url);
-        }
-    }
-
-    @Override
-    public void onPageFinished(WebView view, String url) {
-        super.onPageFinished(view, url);
-        if(cjsActionDispatcher!=null){
-            cjsActionDispatcher.onPageFinished(view, url);
-        }
     }
 
 
-    private void executeJavascript(WebView webView, String js) throws CJSBException {
-        CJSBridge2.callH5(webView, js);
+    private void executeJavascript(WebView webView, String js, CJSBridge2.CallH5CallBack callBack) throws CJSBException {
+        CJSBridge2.callH5(webView, js, callBack);
     }
 }
